4924ea54ba8f446db23987408e9b905724389335
meta <meta@DESKTOP-GQP3R9S>
2019-02-15 13:52:05 +0900

    import 수정

--- springA/src/main/java/next/com/mainA.java
+++ springA/src/main/java/next/com/mainA.java
@@ -3,6 +3,8 @@ package next.com;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 
 public class mainA {
@@ -11,6 +13,7 @@ public class mainA {
 
 		// Container 생성
 		ApplicationContext factory = new ClassPathXmlApplicationContext("next/com/beans.xml");
+		
 		
 		// 인자로 주어지는 타입과 빈 이름의 인스턴스를 반환
 		Inout io = factory.getBean("inout", Inout.class);

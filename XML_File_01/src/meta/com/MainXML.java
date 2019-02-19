fc8a200ce666ec788fd2c268ce1fb37ea4fdd7e0
jiyoung4 <46659300+jiyoung4@users.noreply.github.com>
2019-02-19 14:57:13 +0900

    메모리 소요시간 확인 부분 추가

--- XML_File_01/src/meta/com/MainXML.java
+++ XML_File_01/src/meta/com/MainXML.java
@@ -4,8 +4,13 @@ public class MainXML {
 
 	public static void main(String[] args) throws Exception {
 		
+		long preUseMemory = Runtime.getRuntime().totalMemory()-
+				Runtime.getRuntime().freeMemory();
+		
+		
 		long start = System.currentTimeMillis();
 		
+		/*=============================================*/
 		String fileIdsArr[]; // FILE_ID 생성
 		XMLFiles fpfile = new XMLFiles();
 		
@@ -14,11 +19,18 @@ public class MainXML {
 	
 		// FILE_ID를 이용하여 파일 생성 후 결과 파일 생성 실행
 		fpfile.changeInfo(fileIdsArr);
+		/*=============================================*/
 		
 		long end = System.currentTimeMillis();
+
+		long aftUseMemory = Runtime.getRuntime().totalMemory()-
+				Runtime.getRuntime().freeMemory();
+		
+		long useMemory = (aftUseMemory - preUseMemory);
 		
 		//System.out.println((end-start)/1000.0 +"초");
-		System.out.println((end-start) +"ms");
+		System.out.println(String.format("%,d",(end-start)) +"ms");
+		System.out.println("Used Memory: "+String.format("%,d", useMemory/1000)+" kbytes");
 		
 	}
 
